b83ceab73b3663045b6dfcbfc173b1c904d98403
AJ Call <AJCall710@gmail.com>
2018-06-11 13:51:58 -0600

    created a post component

--- src/components/recentPosts.js
+++ src/components/recentPosts.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Post from './post';
 
 import * as actions from '../actions';
 
@@ -13,9 +14,7 @@ class RecentPosts extends Component {
         const posts = this.props.RecentPosts.map((post, index) => {
             if(index < 3) {
                 return (
-                    <li key={index}>
-                    {post.title}
-                    </li>
+                    <Post {...post} key={index} />
                 )
             }
         })
